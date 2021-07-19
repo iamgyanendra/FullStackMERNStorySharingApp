2fa29d1b70163567c8ed0c7dce92c90a7b7c44d0
Gyandendra Prakash <prakash9.2020@gmail.com>
2021-07-19 13:09:21 +0530

    bug removed

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 app.use(express.json({limit: '30mb', extended: true }));
 app.use(express.urlencoded({limit: '30mb', extended: true}));
-app.use('*', cors());
+app.use(cors());
 
 // var corsOptions = {
 //     origin: '*',
