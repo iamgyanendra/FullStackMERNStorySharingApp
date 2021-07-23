85fbe2a0bf9cba662c01e26f2ced1e8deb3b8005
Gyandendra Prakash <prakash9.2020@gmail.com>
2021-07-23 09:57:40 +0530

    improvements

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 //const url = 'https://story-sharing-app.herokuapp.com/posts';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://story-sharing-app.herokuapp.com/posts' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
